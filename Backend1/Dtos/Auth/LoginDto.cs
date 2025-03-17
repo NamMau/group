211dc1999f63669e12a751ce06520b69f890fd8c
Mầu Tiến Nam <130735681+NamMau@users.noreply.github.com>
2025-03-18 00:33:26 +0700

    Delete Backend1 directory

--- Backend1/Dtos/Auth/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend1.Dtos.Auth
-{
-    public class LoginDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-
-    }
-}
--- Backend1/Services/ITokenService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Backend1.Services
-{
-    public interface ITokenService
-    {
-        string GenerateToken(int userId, string email, string role);
-    }
-}
